06dcaab755106c55230ae3cf847acd4cfdc5056c
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-05-03 19:54:27 -0300

    create: base project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { GlobalStyles } from "./styles/global"
+
+function App() {
+
+    return (
+        <>
+            <GlobalStyles />
+            <h1>Hello world</h1>
+        </>
+
+    )
+}
+
+export default App
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+export default {
+    colors: {
+        shape: "#FAFAFA",
+        black: "#121212",
+        purple: "#8257E5",
+        green: "#04d361"
+    }
+}
